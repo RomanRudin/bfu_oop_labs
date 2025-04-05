1b8b184a7e22cda828913f5e614e6c6c1efc9923
Roman <rudin.roman@bk.ru>
2025-04-05 16:33:02 +0200

    Lab3 was done without syslog because I'm on Windows

---
+++ Labs/Lab3/socket_server.py
@@ -0,0 +1,35 @@
+import socket
+
+def run_tcp_server():
+    HOST = 'localhost'  # Слушаем все интерфейсы: '' 
+    PORT = 5140
+    
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        print(f"TCP Server listening on {HOST}:{PORT}")
+        
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                print(f"Connected by {addr}")
+                data = conn.recv(1024)
+                if data:
+                    print(f"Received TCP: {data.decode('utf-8')}")
+
+
+def run_udp_server():
+    HOST = 'localhost'
+    PORT = 5140
+    
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.bind((HOST, PORT))
+        print(f"UDP Server listening on {HOST}:{PORT}")
+        
+        while True:
+            data, addr = s.recvfrom(1024)
+            print(f"Received UDP from {addr}: {data.decode('utf-8')}")
+
+
+if __name__ == "__main__":
+    run_tcp_server()
